691840fcb956121770a25b35af91f4a0c1d40146
misheck gogo <misheckgogo45@gmail.com>
2024-02-27 14:32:50 -0800

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,19 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t rightheight,size_t leftheight = 0;
+	size_t rightheight == 0;
+	size_t leftheight == 0;
 
     if (tree == NULL)
         return (0);
 
     if (tree == left)
     size_t left_height = binary_tree_height(tree->left);
+
 	if (tree == right)
     size_t right_height = binary_tree_height(tree->right);
 
-    if(left_height > right_height)
+    if(left_height > right_height);
+
 	return(right_height) + 1;
 }
