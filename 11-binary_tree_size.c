e73c920a201fb3e6bcb702e82e21bd0ed77e5a7d
misheck gogo <misheckgogo45@gmail.com>
2024-02-28 06:18:16 -0800

    Write a function that measures the size of a binary tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measures size of binary tree
+ * @tree: pointer the root node of the tree
+ * Return: 0 if the tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	size_t left_size = binary_tree_size(tree->left);
+	size_t right_size = binary_tree_size(tree->right);
+
+	return (left_size + right_size + 1);
+}
